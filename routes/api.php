ee18053d74bcb2f91131d1d04ed1ed0a8a034bf2
Bruno Oliveira <bds.commus@gmail.com>
2024-02-20 22:32:00 -0300

    add first endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/', function () {
+    return response()->json([
+        'PHP' => PHP_VERSION,
+        'Laravel' => Illuminate\Foundation\Application::VERSION,
+        'Owner' => 'PHP PIAUÍ',
+        'What' => '2º PHPPUB',
+        'Where' => 'TapHouse',
+        'When' => new DateTime('2024-02-21 19:30:00', new DateTimeZone('America/Fortaleza')),
+        'Title' => 'CI/CD with GitHub Actions',
+        'Details' => 'Create a pipeline for CI/CD using enviroment variables with GitHub Actions do deploy a Laravel project in a VPS.',
+    ], Response::HTTP_OK);
 });
