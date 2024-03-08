56c53afeb0dc33f1d5da0d4b9f10cf73357343ef
Bruno Oliveira <bds.commus@gmail.com>
2024-03-07 21:22:39 -0300

    add step

--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,23 @@ Route::get('/', function () {
         'PHP' => PHP_VERSION,
         'Laravel' => Illuminate\Foundation\Application::VERSION,
         'Owner' => 'PHP PIAUÍ',
+        'Made By' => 'Bruno Oliveira (https://github.com/BdsOliveira)',
         'What' => '2º PHPPUB',
         'Where' => 'TapHouse',
-        'When' => new DateTime('2024-02-21 19:30:00', new DateTimeZone('America/Fortaleza')),
-        'Title' => 'CI/CD with GitHub Actions',
+        'When' => [
+            'Planned' => new DateTime('2024-02-20 19:30:00', new DateTimeZone('America/Fortaleza')),
+            'Happening' => new DateTime('2024-03-07 19:30:00', new DateTimeZone('America/Fortaleza')),
+        ],
+        'Title' => 'Pipeline CI/CD with GitHub Actions',
         'Details' => 'Create a pipeline for CI/CD using enviroment variables with GitHub Actions do deploy a Laravel project in a VPS.',
+        'GitHub' => 'https://github.com/php-piaui/pipeline',
+        'Extra Info' => [
+            'Notion Outline' => 'https://dear-romano-1d6.notion.site/Pipeline-CI-CD-com-GitHub-Actions-02bcf3c15ba24ccc9a88f7b2479d6c8f',
+            'Enviroments' => [
+                'Development' => 'https://pipelinedevelop.phppiaui.com.br/',
+                'Homologation' => 'https://pipelinehomologacao.phppiaui.com.br/',
+                'Production' => 'https://pipeline.phppiaui.com.br/',
+            ],
+        ],
     ], Response::HTTP_OK);
 });
